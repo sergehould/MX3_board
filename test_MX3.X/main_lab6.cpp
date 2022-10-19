1fad07cafcf237195e0111f73e053657dc07e354
sergehould <houlds@vaniercollege.qc.ca>
2022-10-19 11:25:24 -0400

    Add files via upload

---
+++ test_MX3.X/main_lab6.cpp
@@ -0,0 +1,36 @@
+/**
+   	FileName:     
+	
+ 
+	Description: 
+      
+ * * REVISION HISTORY:
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Author        	Date            Comments on this revision
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Serge Hould      
+ * 	TODO: 
+ * 		
+ *******************************************************************************/
+#include <xc.h>
+#include <stdio.h>
+#include "util.h"
+#include "console.h"
+#include "initBoard.h"
+#include "keyb.h"
+
+
+int main( void){
+    setbuf(stdout, NULL); 
+    UART_InitPoll(460800);
+    set_stdio(C_UART4);
+    printf("Hello world\n");
+    
+    /* Infinite loop */
+    while (1) {
+        // your code here
+    } //while
+    return 0;
+}// main
+
+
