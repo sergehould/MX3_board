9791f5175f18a16f9e8d2411cbfd58d658c8fc7d
sergehould <houlds@vaniercollege.qc.ca>
2023-10-18 11:57:53 -0400

    Update util.h

--- test_MX3.X/util.h
+++ test_MX3.X/util.h
@@ -3,6 +3,7 @@
  * Author: sh
  *
  * Created on September 22, 2021, 5:59 PM
+ * Serge Hould      28 Feb. 2022     Add heartbeat_init macro
  */
 
 #ifndef UTIL_H
@@ -11,6 +12,8 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
+#define 	heartbeat_init  init_heartbeat
+#define		tone_init			init_tone
 
 void heartbeat(void);
 void init_heartbeat(void);
@@ -19,6 +22,8 @@ void tone_low(int cnt);
 void init_tone(void);
 void delay_ms( unsigned int);
 void delay_10us( unsigned int);
+long map(long x, long in_min, long in_max, long out_min, long out_max);
+
 
 #ifdef	__cplusplus
 }
